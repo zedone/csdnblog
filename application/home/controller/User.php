e2a17fa044f5b744d09852fdf7901ae226a63e90
userchen23 <1243046868@qq.com>
2019-06-01 16:22:37 +0800

    分工部分完成

--- application/home/controller/User.php
+++ application/home/controller/User.php
@@ -4,6 +4,8 @@ namespace app\home\controller;
 
  use \think\Controller;
  use \app\home\model\User as UserModel;
+ use \app\home\model\Token as TokenModel;
+ use \app\home\model\Blog as BlogModel;
  /**
   * 
   */
@@ -15,6 +17,7 @@ namespace app\home\controller;
         $msg        = '成功';
         $data       = [];
         $post_data  = input('post.');
+
         if (empty($post_data)) {
             $errorno    = 1;
             $msg        = 'post为空';
@@ -36,7 +39,8 @@ namespace app\home\controller;
         }
 
         $user_obj   = new UserModel;
-        $result     = $user_obj->getinfo('phone',$phone);
+        $result     = $user_obj->getInfo('phone',$phone);
+
         if ($result) {
             $errorno    = 4;
             $msg        = '电话号已被使用';
@@ -54,6 +58,7 @@ namespace app\home\controller;
             'update_time'    => $time,
         ];
         $result     = $user_obj-> add($tmp_data);
+
         if (!$result) {
             $errorno    = 5;
             $msg        = '插入失败';
@@ -64,4 +69,99 @@ namespace app\home\controller;
 
     }  
 
+    public function saveBlog(){
+        $errorno    = 0;
+        $msg        = '成功';
+        $data       = [];
+        $post_data  = input('post.');
+
+        if (empty($post_data)) {
+            $errorno    = 1;
+            $msg        = 'post为空';
+            sendJson($errorno,$msg,$data);die();
+        }
+        $token      = !empty($post_data['token'])?$post_data['token']:'';
+        $token_obj  = new TokenModel;
+        $result      = $token_obj->getUserInfo($token);
+
+        if (!$result) {
+            $errorno    = 2;
+            $msg        = 'token不存在或失效';
+            sendJson($errorno,$msg,$data);die();            
+        }
+        $user_id        = $result['value']['id'];
+
+//id,title,classify_id,content,status,create_time,update_time
+        $title          = !empty($post_data['title'])?$post_data['title']:'';
+        $classify_id    = !empty($post_data['classify_id'])?$post_data['classify_id']:0;
+        $content        = !empty($post_data['content'])?$post_data['content']:'';
+
+        $time =time();
+        $tmp_data = [
+            'user_id'       => $user_id,
+            'title'         => $title,
+            'classify_id'   => $classify_id,
+            'content'       => $content,
+            'status'        => 1,
+            'create_time'   => $time,
+            'update_time'   => $time,
+        ];        
+        
+        $blog_obj   = new BlogModel;
+        $result     = $blog_obj-> add($tmp_data);
+
+        if (!$result) {
+            $errorno    = 5;
+            $msg        = '插入失败';
+            sendJson($errorno,$msg);die();
+        }
+
+        sendJson();die();     
+    }
+
+    public function blogList(){
+        $errorno    = 0;
+        $msg        = '成功';
+        $data       = [];
+        $post_data  = input('post.');
+
+        if (empty($post_data)) {
+            $errorno    = 1;
+            $msg        = 'post为空';
+            sendJson($errorno,$msg,$data);die();
+        }
+
+        $token      = !empty($post_data['token'])?$post_data['token']:'';
+        $token_obj  = new TokenModel;
+        $result      = $token_obj->getUserInfo($token);
+
+        if (!$result) {
+            $errorno    = 2;
+            $msg        = 'token不存在或失效';
+            sendJson($errorno,$msg,$data);die();            
+        }
+        $user_id        = $result['value']['id'];
+
+        $blog_obj   = new BlogModel;
+        $result     = $blog_obj->selectInfo('user_id',$user_id);
+
+        $classify_obj   = new ClassifyModel;
+        $class_info     = $classify_obj->getInfo('id',$result['classify_id']);
+        $class_name     = !empty($class_info['name'])?$class_info['name']:'';
+
+        $data       = [
+            'classify_id'   => $result['classify_id'],
+            'class_name'    => $class_name,
+            'title'         => $result['title'],
+            'content'       => $result['content'],
+        ];
+
+        if (!$result) {
+            $errorno    = 3;
+            $msg        = '查询失败';
+            sendJson($errorno,$msg);die();
+        }
+
+        sendJson($errorno,$msg,$data);die();         
+    }
  }
