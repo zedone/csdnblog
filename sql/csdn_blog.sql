2fda93ce11ad5dfd44ccc5a1a808e4507758cc91
userchen23 <1243046868@qq.com>
2019-06-01 10:32:47 +0800

    sql文件

---
+++ sql/csdn_blog.sql
@@ -0,0 +1,68 @@
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: csdn_blog
+Target Host: localhost
+Target Database: csdn_blog
+Date: 2019/6/1 10:27:38
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for blog
+-- ----------------------------
+DROP TABLE IF EXISTS `blog`;
+CREATE TABLE `blog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `pic` varchar(255) NOT NULL DEFAULT '',
+  `content` varchar(2000) NOT NULL DEFAULT '',
+  `status` int(2) NOT NULL,
+  `create_time` int(20) NOT NULL,
+  `update_time` int(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for classify
+-- ----------------------------
+DROP TABLE IF EXISTS `classify`;
+CREATE TABLE `classify` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `status` int(2) NOT NULL,
+  `create_time` int(20) NOT NULL,
+  `update_time` int(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for token
+-- ----------------------------
+DROP TABLE IF EXISTS `token`;
+CREATE TABLE `token` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(255) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `expire_time` int(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `phone` int(11) NOT NULL,
+  `password` varchar(50) NOT NULL DEFAULT '',
+  `status` int(2) NOT NULL,
+  `create_time` int(20) NOT NULL,
+  `updatetime` int(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records 
+-- ----------------------------
